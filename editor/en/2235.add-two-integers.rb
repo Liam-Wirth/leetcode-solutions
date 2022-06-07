96c11453cd0426d6c00f43a2ae99c17d9d73c630
Liam-Wirth <liam.wirth1@outlook.com>
2022-06-06 23:48:40 -0700

    first ruby

---
+++ editor/en/2235.add-two-integers.rb
@@ -0,0 +1,52 @@
+#
+# @lc app=leetcode id=2235 lang=ruby
+#
+# [2235] Add Two Integers
+#
+# https://leetcode.com/problems/add-two-integers/description/
+#
+# algorithms
+# Easy (92.91%)
+# Likes:    198
+# Dislikes: 783
+# Total Accepted:    37K
+# Total Submissions: 39.8K
+# Testcase Example:  '12\n5'
+#
+# Given two integers num1 and num2, return the sum of the two integers.
+# 
+# Example 1:
+# 
+# 
+# Input: num1 = 12, num2 = 5
+# Output: 17
+# Explanation: num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is
+# returned.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: num1 = -10, num2 = 4
+# Output: -6
+# Explanation: num1 + num2 = -6, so -6 is returned.
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# -100 <= num1, num2 <= 100
+# 
+# 
+#
+
+# @lc code=start
+# @param {Integer} num1
+# @param {Integer} num2
+# @return {Integer}
+def sum(num1, num2)
+     return num1 + num2
+end 
+# @lc code=end
+
