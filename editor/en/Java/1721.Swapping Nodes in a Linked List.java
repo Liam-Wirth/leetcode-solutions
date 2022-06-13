49f4c1795b5fe2ee2c4619ad5f2eb13818e1c816
Liam-Wirth <liam.wirth1@outlook.com>
2022-06-12 19:05:29 -0700

    refactoring shid

--- editor/en/Java/1721.Swapping Nodes in a Linked List.java
+++ editor/en/Java/1721.Swapping Nodes in a Linked List.java
@@ -2,10 +2,7 @@ package Java;
 
 
   import java.util.ArrayList;
-  import java.util.Arrays;
   import java.util.LinkedList;
-  import java.util.List;
-  import java.util.function.Consumer;
 
   class SwappingNodesInALinkedList{
       public static void main(String[] args) {
