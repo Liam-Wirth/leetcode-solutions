4cf0fb8c6e69fbb370fc6a0a61a18a9d5d7e80b6
Liam-Wirth <liam.wirth1@outlook.com>
2022-06-13 20:59:23 -0700

    rubocop makes me want to bash my head in

--- editor/en/Ruby/13.roman-to-integer.rb
+++ editor/en/Ruby/13.roman-to-integer.rb
@@ -85,19 +85,31 @@
 # @param {String} s
 # @return {Integer}
 def roman_to_int(s)
-  arr = s.chars
-  arr.each_with_index do |char, index|
-    case char
-    when 'I'
-
-    when 'V'
-    when 'X'
-    when 'L'
-    when 'C'
-    when 'D'
-    when 'M'
-    end
-  end
+hash = {
+    'I' => 1,
+    'V' => 5,
+    'X' => 10,
+    'L' => 50,
+    'C' => 100,
+    'D' => 500,
+    'M' => 1000
+}
+fuck_you_rubocop(s, hash)
 end
-# @lc code=end
 
+def fuck_you_rubocop(s, hash)
+  out = 0
+  i = 0
+while i < s.length
+  if i + 1 < s.length && hash[s[i]] < hash[s[i + 1]]
+    out += hash[s[i + 1]] - hash[s[i]]
+        i+=1
+     else
+        out += hash[s[i]]
+     end
+    i += 1
+   end
+  out
+end
+
+# @lc code=end
