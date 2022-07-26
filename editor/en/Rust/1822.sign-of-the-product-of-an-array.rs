8761629816aafdaaad9a9ecb35c5ddbaa4c804fd
Liam-Wirth <liam.wirth1@outlook.com>
2022-07-26 14:05:35 -0700

    f

---
+++ editor/en/Rust/1822.sign-of-the-product-of-an-array.rs
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode id=1822 lang=rust
+ *
+ * [1822] Sign of the Product of an Array
+ *
+ * https://leetcode.com/problems/sign-of-the-product-of-an-array/description/
+ *
+ * algorithms
+ * Easy (67.03%)
+ * Likes:    653
+ * Dislikes: 87
+ * Total Accepted:    117.1K
+ * Total Submissions: 175K
+ * Testcase Example:  '[-1,-2,-3,-4,3,2,1]'
+ *
+ * There is a function signFunc(x) that returns:
+ * 
+ * 
+ * 1 if x is positive.
+ * -1 if x is negative.
+ * 0 if x is equal to 0.
+ * 
+ * 
+ * You are given an integer array nums. Let product be the product of all
+ * values in the array nums.
+ * 
+ * Return signFunc(product).
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [-1,-2,-3,-4,3,2,1]
+ * Output: 1
+ * Explanation: The product of all values in the array is 144, and
+ * signFunc(144) = 1
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [1,5,0,2,-3]
+ * Output: 0
+ * Explanation: The product of all values in the array is 0, and signFunc(0) =
+ * 0
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: nums = [-1,1,-1,1,-1]
+ * Output: -1
+ * Explanation: The product of all values in the array is -1, and signFunc(-1)
+ * = -1
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 1000
+ * -100 <= nums[i] <= 100
+ * 
+ * 
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn array_sign(nums: Vec<i32>) -> i32 {
+        
+    }
+}
+// @lc code=end
+
