b8495b324dcc25d3c12180f7dacc25c3f6e3dbe3
liam <liam.wirth1@outlook.com>
2022-11-08 17:00:04 -0700

    setting up 26

---
+++ editor/en/rust/26.remove-duplicates-from-sorted-array.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        //nums is non-decreasing order
+        //assume the size of the array is Immutable
+            //IE:
+        //input
+            // nums = [0,1,2,2,2,3,4]
+        //output
+            // nums = [0,1,2,3,4,0,0]
+    }
+}
