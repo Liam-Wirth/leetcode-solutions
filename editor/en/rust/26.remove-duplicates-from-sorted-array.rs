30518cabb318973b32108bed75b6cfcff1fd13b7
liam <liam.wirth1@outlook.com>
2022-11-11 23:57:02 -0700

    :(

--- editor/en/rust/26.remove-duplicates-from-sorted-array.rs
+++ editor/en/rust/26.remove-duplicates-from-sorted-array.rs
@@ -1,3 +1,7 @@
+
+
+//TODO
+//TODO figure out how to get todo comments to stfu
 impl Solution {
     pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
         //nums is non-decreasing order
@@ -12,5 +16,14 @@ impl Solution {
         * */ 
         //NOTE the array is not returned, it's just modified, only return the amount of changes
         //made
+        let mut lastNum:i32 = 0;
+        let mut k:i32 = 0;
+        for(i,num) in nums.iter().enumerate() {
+            if i!=0 {
+                assert_eq!(nums[i-1],num );
+            
+            }
+        }
     }
 }
+
