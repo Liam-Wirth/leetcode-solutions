2fee1c0e77e2556b9a3231238d477931fa7e9141
Liam <61883659+Liam-Wirth@users.noreply.github.com>
2024-01-16 23:11:59 -0800

    Create insert_delete_get_random_380.rs
    did this from odder pc

---
+++ rust/src/insert_delete_get_random_380.rs
@@ -0,0 +1,42 @@
+use std::{collections::HashMap, vec};
+use rand::prelude::*;
+struct RandomizedSet {
+    list: Vec<i32>,
+    set: HashMap<i32, usize>, 
+}
+
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl RandomizedSet {
+
+    fn new() -> Self {
+        Self { list: Vec::new(), set: HashMap::new() }
+    }
+    
+    fn insert(&mut self, val: i32) -> bool {
+        if !self.set.contains_key(&val) {
+            self.set.insert(val, self.list.len());
+            self.list.push(val);
+            true
+        } else {false}
+    }
+    
+    fn remove(&mut self, val: i32) -> bool {
+        if let Some(index) = self.set.remove(&val) {
+            if let Some(&val2) = self.list.last() {
+                self.list.swap_remove(index);
+                if val != val2 {
+                    *self.set.get_mut(&val2).unwrap() = index;
+                }
+            }
+            true
+        } else { false } 
+    }
+    
+    fn get_random(&mut self) -> i32 {
+        *self.list.choose(&mut rand::thread_rng()).unwrap()
+    }
+}
