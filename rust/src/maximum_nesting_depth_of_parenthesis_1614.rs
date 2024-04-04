25e7cd892a68b86ca5cb7228383198be2a276591
liam <liam.wirth1@outlook.com>
2024-04-04 00:19:10 -0700

    job still running

--- rust/src/maximum_nesting_depth_of_parenthesis_1614.rs
+++ rust/src/maximum_nesting_depth_of_parenthesis_1614.rs
@@ -1,31 +1,30 @@
 use crate::Solution;
 impl Solution {
     pub fn max_depth(s: String) -> i32 {
-      //this is very simmilar to another problem I've solved, we are gonna use a stack
-      let mut depth: Vec<char> = Vec::new(); 
-      let mut max: usize = 0; 
-       //WE GREEDY IN THIS MOTHAFUCKA!
-       if s.len() == 0 {
-        return 0;
-      }
-      //else {      <--- commented out else block cause it's an old habbit and not sure if  I should keep doing it
-    for ch in s.chars() {
-        match (ch) {
-            '(' => {
-                depth.push(ch);
-                if max < depth.len(){
-                    max = depth.len();
-                }
-            },
-            ')' => {
-                depth.pop();
-            },
-            _ => {
-                continue; // we don't care!!
-            },
+        //this is very simmilar to another problem I've solved, we are gonna use a stack
+        let mut depth: Vec<char> = Vec::new();
+        let mut max: usize = 0;
+        //WE GREEDY IN THIS MOTHAFUCKA!
+        if s.len() == 0 {
+            return 0;
         }
-    }
+        //else {      <--- commented out else block cause it's an old habbit and not sure if  I should keep doing it
+        for ch in s.chars() {
+            match (ch) {
+                '(' => {
+                    depth.push(ch);
+                    if max < depth.len() {
+                        max = depth.len();
+                    }
+                }
+                ')' => {
+                    depth.pop();
+                }
+                _ => {
+                    continue; // we don't care!!
+                }
+            }
+        }
         max as i32
     }
-
 }
---
+++ rust/src/word_search_79.rs
@@ -0,0 +1,62 @@
+use crate::Solution;
+impl Solution {
+    pub fn exist(mut board: Vec<Vec<char>>, word: String) -> bool {
+        let word: Vec<char> = word.chars().collect();
+
+        for i in 0..board.len() {
+            for j in 0..board[0].len() {
+                if Solution::dfs(&mut board, &word, i, j) {
+                    return true;
+                }
+            }
+        }
+
+        false
+    }
+
+    /*
+    technically popping values off of the word vector values we've seen????
+    word[1..]
+    base
+    case word.len() == 1 {
+        return board[x][y] == word[0]
+    }
+
+
+    */
+
+    fn dfs(board: &mut Vec<Vec<char>>, word: &[char], x: usize, y: usize) -> bool {
+        if word.len() == 1 {
+            return board[x][y] == word[0];
+        }
+        //some other basecase me thinks?
+        if board[x][y] != word[0] {
+            return false;
+        }
+        let neighborhood = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+        board[x][y] = '~';
+
+        for dir in neighborhood {
+            let x = x as i32 + dir.0;
+            let y = y as i32 + dir.1;
+            if x < 0 || y < 0 {
+                continue;
+            };
+            let x = x as usize;
+            let y = y as usize;
+
+            if x >= board.len() || y >= board[0].len() {
+                continue;
+            }
+            if board[x][y] == '~' {
+                continue;
+            }
+            if Solution::dfs(board, &word[1..], x, y) {
+                board[x][y] = word[0];
+                return true;
+            }
+        }
+        board[x][y] = word[0];
+        false
+    }
+}
