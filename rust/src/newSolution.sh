e9b037ed5bd600192ee9c96ecc8bdf7c3b456f8d
liam <liam.wirth1@outlook.com>
2024-04-04 01:34:48 -0700

    Bash script just needs to have case sensitivity fixed now

--- rust/src/newSolution.sh
+++ rust/src/newSolution.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
+# Function to format problem name
+format_problem_name() {
+    name=$(echo "$1" | tr ' ' '_')
+    echo "$name"
+}
+clean_response() {
+    response=$(echo "$1" | tr -d '[:space:]' | tr -d '[:punct:]' | tr '[:upper:]' '[:lower:]')
+    echo "$response"
+}
+
 # Ask user for the problem name
 echo "Tell me the problem name:"
 read problem_name
-
-# Remove whitespace and replace spaces with underscores
-problem_name=$(echo "$problem_name" | tr -d '[:space:]' | tr ' ' '_')
+formatted_name=$(format_problem_name "$problem_name")
 
 # Ask user for the problem number
 echo "Tell me the problem number:"
 read problem_number
+cleaned_number=$(clean_response "$problem_number")
 
 # Construct the filename
-filename="${problem_name}_${problem_number}.rs"
+filename="${problem_name// /_}_${cleaned_number}.rs"
 
 # Create the file
 touch "./$filename"
@@ -21,4 +30,13 @@ echo "mod $filename" >> lib.rs
 
 echo "File created: ./$filename"
 echo "mod $filename added to lib.rs"
+
+echo "Open $filename in Vim? (y\n)"
+read response
+cleaned_response=$(clean_response "$response")
+
+# Check if the response is affirmative
+if [[ $cleaned_response == "y" || $cleaned_response == "yes" ]]; then
+   nvim $filename # Open file in nvim editor (you can replace nvim with your preferred editor)
+fi
 
