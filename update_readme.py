f032a1d3bc0396c29ce6e6aacf6d1c7dbc3ddabe
Liam <ltwirth@asu.edu>
2024-06-01 18:13:25 -0700

    .

--- update_readme.py
+++ update_readme.py
@@ -8,6 +8,8 @@ import datetime
 def format_problem_name(name):
     return name.replace('-', ' ').title()
 
+# TODO: Add functionality to this script so that it will replace just the name of the problem with a
+# Markdown fancy link []() (<- like that) in the table, would look nicer I think
 
 
 use_json = True
