c7883c895f01031052742aab03d29c914c81afe2
Liam <liam.wirth1@outlook.com>
2024-07-05 01:14:12 -0700

    imagine solving a leetcode problem after a shot of tequilla, couldn't possibly be me, nope, I deffinitely did NOT do that, I am deffinitely NOT Under the influence right now in any way shape or form

---
+++ python/find_minimum_and_maximum_number_of_nodes_between_critical_points_2058.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+class Solution:
+    def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
+        if not head or not head.next or not head.next.next:
+            return [-1, -1]
+
+        crits = []
+        prev = head
+        curr = head.next
+        pos = 1
+
+        while curr.next:
+            minima = curr.val < prev.val and curr.val < curr.next.val
+            maxima = curr.val > prev.val and curr.val > curr.next.val
+        
+            if minima or maxima:
+                crits.append(pos)
+            prev = curr
+            curr = curr.next
+            pos += 1
+
+        if len(crits) < 2:
+            return [-1, -1]
+            
+        min_dist = float('inf')
+        max_dist = crits[-1] - crits[0]
+
+        for i in range(1, len(crits)):
+            min_dist = min(min_dist, crits[i] - crits[i-1])
+
+        return [min_dist, max_dist]
